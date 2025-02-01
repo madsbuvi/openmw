07abecaff3b1561397b5c8935e0c7bbd88d68f88
Mads Buvik Sandvei <madssandvei@protonmail.com>
2025-02-01 14:48:57 +0100

    working commit

--- apps/openmw/engine.hpp
+++ apps/openmw/engine.hpp
@@ -287,6 +287,7 @@ namespace OMW
 //## VR_PATCH BEGIN
     public:
         void configureVRGraphics(osg::GraphicsContext* gc);
+        void configureVRInputProfiles();
         void configureVRPreScene(const std::filesystem::path& userFile, bool userFileExists,
             const std::filesystem::path& userControllerBindingsFile,
             const std::filesystem::path& controllerBindingsFile);
