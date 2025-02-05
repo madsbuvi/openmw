d93c241b6d0afc97361fce0abadb1e272d75a571
Mads Buvik Sandvei <madssandvei@protonmail.com>
2025-02-05 22:06:45 +0100

    XR Layers working commit

--- apps/openmw/engine.cpp
+++ apps/openmw/engine.cpp
@@ -1259,7 +1259,6 @@ void OMW::Engine::configureVRPreScene(const std::filesystem::path& userFile, boo
     const std::filesystem::path& userControllerBindingsFile, const std::filesystem::path& controllerBindingsFile)
 {
     // Set up enough of VR to view the intro cinematic/loading screen
-    mVrGUIManager = std::make_unique<MWVR::VRGUIManager>(mResourceSystem.get(), mViewer->getSceneData()->asGroup());
     mVrViewer = std::make_unique<VR::Viewer>(mXrSession, mViewer);
     mVrViewer->configureCallbacks();
     auto cullMask = ~(MWRender::VisMask::Mask_UpdateVisitor | MWRender::VisMask::Mask_SimpleWater);
@@ -1268,6 +1267,7 @@ void OMW::Engine::configureVRPreScene(const std::filesystem::path& userFile, boo
     mViewer->getCamera()->setCullMask(cullMask);
     mViewer->getCamera()->setCullMaskLeft(cullMask);
     mViewer->getCamera()->setCullMaskRight(cullMask);
+    mVrGUIManager = std::make_unique<MWVR::VRGUIManager>(mResourceSystem.get(), mViewer->getSceneData()->asGroup());
 
     const std::string xrinputuserdefault = mCfgMgr.getUserConfigPath().string() + "/xrcontrollersuggestions.xml";
     const std::string xrinputlocaldefault = mCfgMgr.getLocalPath().string() + "/xrcontrollersuggestions.xml";
