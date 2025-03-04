7bbf3c22bbd7a08594b36ddc9e30a43ee86dd86f
Mads Buvik Sandvei <madssandvei@protonmail.com>
2025-03-04 22:56:58 +0100

    basic spaces/input framework. lots of action implementations remain to be done.

--- apps/openmw/mwgui/windowmanagerimp.cpp
+++ apps/openmw/mwgui/windowmanagerimp.cpp
@@ -2504,6 +2504,8 @@ namespace MWGui
     {
         mViewer->eventTraversal();
         mViewer->updateTraversal();
+        if (VR::getVR())
+            VR::Session::instance().updateSpaces();
         mViewer->renderingTraversals();
     }
 
@@ -2556,7 +2558,10 @@ namespace MWGui
 
     bool WindowManager::isWindowVisible(std::string_view windowId) const
     {
-        return mLuaIdToWindow.at(windowId)->isVisible();
+        auto it = mLuaIdToWindow.find(windowId);
+        if (it == mLuaIdToWindow.end())
+            throw std::logic_error("Invalid window name: " + std::string(windowId));
+        return it->second->isVisible();
     }
 
     std::vector<std::string_view> WindowManager::getAllWindowIds() const
