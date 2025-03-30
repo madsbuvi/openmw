a1acd03e60187d5a41217024d2e59d20240f2bca
Mads Buvik Sandvei <madssandvei@protonmail.com>
2025-03-30 17:35:50 +0200

    Polish window handling.

--- apps/openmw/mwgui/windowmanagerimp.cpp
+++ apps/openmw/mwgui/windowmanagerimp.cpp
@@ -2164,6 +2164,10 @@ namespace MWGui
 
     void WindowManager::pinWindow(GuiWindow window)
     {
+        if (VR::getVR())
+            // Pinning in VR will need some implementation work
+            return;
+
         switch (window)
         {
             case GW_Inventory:
