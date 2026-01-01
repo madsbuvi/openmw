49eb9b0dff8494a95562e5172dbcf5513d626826
jvoisin <julien.voisin@dustri.org>
2026-01-01 20:57:53 +0000

    Merge branch 'weatherreport' into 'master'
    Improve high wind speed rain/snow behavior (#8864)
    
    Closes #8864
    
    See merge request OpenMW/openmw!5069

--- apps/openmw/mwrender/sky.cpp
+++ apps/openmw/mwrender/sky.cpp
@@ -518,7 +518,7 @@ namespace MWRender
 
     bool SkyManager::getRainRipplesEnabled() const
     {
-        if (!mEnabled || mIsStorm)
+        if (!mEnabled)
             return false;
 
         if (hasRain())
@@ -532,10 +532,7 @@ namespace MWRender
 
     float SkyManager::getPrecipitationAlpha() const
     {
-        if (mEnabled && !mIsStorm && (hasRain() || mParticleNode))
-            return mPrecipitationAlpha;
-
-        return 0.f;
+        return mPrecipitationAlpha;
     }
 
     void SkyManager::update(float duration)
@@ -731,7 +728,7 @@ namespace MWRender
                         = static_cast<osgParticle::ParticleSystem*>(findPSVisitor.mFoundNodes[i]);
 
                     osg::ref_ptr<osgParticle::ModularProgram> program = new osgParticle::ModularProgram;
-                    if (!mIsStorm)
+                    if (occlusionEnabledForEffect)
                         program->addOperator(new WrapAroundOperator(mCamera, defaultWrapRange));
                     program->addOperator(new WeatherAlphaOperator(mPrecipitationAlpha, false));
                     program->setParticleSystem(ps);
