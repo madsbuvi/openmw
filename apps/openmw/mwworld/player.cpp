e589f967ad2c96ad323620c8a73d004e11ff131f
Alexei Kotov <alexdobrohotov@yandex.ru>
2026-02-27 13:53:21 +0300

    Merge branch 'overgrownweres' into 'master'
    Reset modifiers before applying werewolf modifiers
    
    Closes #8975
    
    See merge request OpenMW/openmw!5161

--- apps/openmw/mwworld/player.cpp
+++ apps/openmw/mwworld/player.cpp
@@ -114,7 +114,8 @@ namespace MWWorld
         for (const auto& attribute : store->get<ESM::Attribute>())
         {
             MWMechanics::AttributeValue value = npcStats.getAttribute(attribute.mId);
-            value.setModifier(attribute.mWerewolfValue - value.getModified());
+            value.setBase(value.getBase(), true);
+            value.setModifier(attribute.mWerewolfValue - value.getBase());
             npcStats.setAttribute(attribute.mId, value);
         }
 
@@ -125,7 +126,8 @@ namespace MWWorld
                 continue;
 
             MWMechanics::SkillValue& value = npcStats.getSkill(skill.mId);
-            value.setModifier(skill.mWerewolfValue - value.getModified());
+            value.setBase(value.getBase(), true);
+            value.setModifier(skill.mWerewolfValue - value.getBase());
         }
     }
 
