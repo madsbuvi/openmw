a6fd8f72b1934987f8e7ab2dea690dc95d90b476
Alexei Kotov <alexdobrohotov@yandex.ru>
2026-03-07 17:47:57 +0300

    Merge branch 'staticgeneration' into 'master'
    Implement createRecordDraft for statics
    
    Closes #8998
    
    See merge request OpenMW/openmw!5178

--- apps/openmw/mwstate/statemanagerimp.cpp
+++ apps/openmw/mwstate/statemanagerimp.cpp
@@ -552,6 +552,7 @@ void MWState::StateManager::loadGame(const Character* character, const std::file
                 case ESM::REC_CREA:
                 case ESM::REC_CONT:
                 case ESM::REC_RAND:
+                case ESM::REC_STAT:
                     MWBase::Environment::get().getWorld()->readRecord(reader, n.toInt());
                     break;
 
