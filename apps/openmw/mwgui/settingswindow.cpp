bedf05e2d785420cbfccca7b8843e80f5aff22cf
Alexei Kotov <alexdobrohotov@yandex.ru>
2026-03-24 22:53:58 +0300

    Merge branch 'clamplighting' into 'master'
    In-game checkbox for Clamp Lighting
    
    See merge request OpenMW/openmw!5218

--- apps/openmw/mwgui/settingswindow.cpp
+++ apps/openmw/mwgui/settingswindow.cpp
@@ -688,6 +688,7 @@ namespace MWGui
 
         Settings::shaders().mForcePerPixelLighting.reset();
         Settings::shaders().mClassicFalloff.reset();
+        Settings::shaders().mClampLighting.reset();
         Settings::shaders().mMatchSunlightToSun.reset();
         Settings::shaders().mLightBoundsMultiplier.reset();
         Settings::shaders().mMaximumLightDistance.reset();
